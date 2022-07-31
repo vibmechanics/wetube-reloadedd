2950322249de5c6d0a62580806e846da8934a228
vibmechanics <vibmechanic@gmail.com>
2022-07-31 19:25:35 +0900

    #3.5 Middlewares part One

--- src/server.js
+++ src/server.js
@@ -4,14 +4,15 @@ const PORT = 4000;
 
 const app = express();
 
+const gossipMiddleware = (req, res, next) => {
+    console.log("I'm in the middle");
+    next();
+};
+
 const handleHome = (req, res) => {
-    return res.send("I still love you");
+    return res.send("I live middleware");
 };
-const handleLogin = (req, res) => {
-    return res.send("Login here.");
-};
-app.get("/", handleHome);
-app.get("/login", handleLogin);
+app.get("/", gossipMiddleware, handleHome);
 
 const handleListening = () =>
     console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
