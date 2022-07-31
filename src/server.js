179925d7d6db9e6f3867870fa9549a33517f0b45
vibmechanics <vibmechanic@gmail.com>
2022-07-31 19:40:38 +0900

    #3.6 Middlewares part Two

--- src/server.js
+++ src/server.js
@@ -4,15 +4,32 @@ const PORT = 4000;
 
 const app = express();
 
-const gossipMiddleware = (req, res, next) => {
-    console.log("I'm in the middle");
+const logger = (req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+};
+
+const privateMiddleware = (req, res, next) => {
+    const url = req.url;
+    if (url === "/protected") {
+        return res.send("<h1>Not Allowed</h1>");
+    }
+    console.log("Allowed, you may continue.");
     next();
 };
 
 const handleHome = (req, res) => {
     return res.send("I live middleware");
 };
-app.get("/", gossipMiddleware, handleHome);
+
+const handleProtected = (req, res) => {
+    return res.send("Welcone to the private lounge.");
+};
+
+app.use(logger);
+app.use(privateMiddleware);
+app.get("/", handleHome);
+app.get("/protected", handleProtected);
 
 const handleListening = () =>
     console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
