e7a7df927d18bf41a735f996a94914cc6a2551a1
vibmechanics <vibmechanic@gmail.com>
2022-08-03 00:43:32 +0900

    #5.4 Variables to Templates

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,11 +1,15 @@
 export const trending = (req, res) => {
-    res.render("home");
+    res.render("home", { pageTitle: "Home" });
 };
 export const see = (req, res) => {
-    res.render("watch");
+    console.log(res.req.params.id);
+    res.render("watch", {
+        pageTitle: "Watch",
+        id: `${res.req.params.id}`,
+    });
 };
 export const edit = (req, res) => {
-    res.render("edit");
+    res.render("edit", { pageTitle: "Edit" });
 };
 export const search = (req, res) => res.send("Search Video");
 export const upload = (req, res) => res.send("Upload Video");
